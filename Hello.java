a8d61108ca52cebd000880e4e8c7b95d077fc3f1
Joshua Olson <39246349+JoshuaOlson@users.noreply.github.com>
2019-04-20 13:58:54 -0500

    Added new file Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+//Joshua Hello World program written in VS Code
+
+import java.lang.*;
+
+class Hello{
+    public static void main(String args[]) {
+        System.out.println("Hello Joshua");        
+    }
+}
